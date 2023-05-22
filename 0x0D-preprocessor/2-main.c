06c8d00a32e2664554620d84a7153c598d3ae462
Azizipho Madolo <aziziphomadolo@gmail.com>
2023-05-22 13:07:21 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char *filename = __FILE__;
+	
+	printf("%s\n", filename);
+	
+	return 0;
+}
